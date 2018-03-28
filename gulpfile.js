5fb451f284d1fc29c807e4db3fabecbdce567e09
muoitm <muoimouse@gmail.com>
2018-03-28 09:35:44 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,23 @@
 const gulp = require("gulp");
 const nodemon = require("gulp-nodemon");
-const plumber = require("gulp-plumber");
+// const plumber = require("gulp-plumber");
 const livereload = require("gulp-livereload");
 
-
 gulp.task("develop", () => {
-    livereload.listen();
-    nodemon({
-        script: "app.js",
-        ext: "js coffee ejs",
-        stdout: false
-    }).on("readable", function () {
-        this.stdout.on("data", (chunk) => {
-            if (/^Express server listening on port/.test(chunk)) {
-                livereload.changed(__dirname);
-            }
-        });
-        this.stdout.pipe(process.stdout);
-        this.stderr.pipe(process.stderr);
+  livereload.listen();
+  nodemon({
+    script: "app.js",
+    ext: "js coffee ejs",
+    stdout: false
+  }).on("readable", function() {
+    this.stdout.on("data", chunk => {
+      if (/^Express server listening on port/.test(chunk)) {
+        livereload.changed(__dirname);
+      }
     });
+    this.stdout.pipe(process.stdout);
+    this.stderr.pipe(process.stderr);
+  });
 });
 
-gulp.task("default", [
-    "develop"
-]);
+gulp.task("default", ["develop"]);
